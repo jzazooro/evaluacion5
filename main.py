ddf8a83593ef61f214a77314b91e075345a77054
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-12-20 10:21:54 +0100

    terino el ejercicio2 y creo su mai

---
+++ main.py
@@ -0,0 +1,11 @@
+from ejercicio2.contador import*
+
+def main2():
+    contador('inc')
+    contador('inc')
+    contador('inc')
+    contador('dec')
+    contador('')
+    contador('dec')
+
+main2()
