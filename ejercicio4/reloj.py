15f6422f6959937258e6db5e4604ff7654151668
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-12-20 11:58:15 +0100

    cambio en el reloj

--- ejercicio4/reloj.py
+++ ejercicio4/reloj.py
@@ -4,8 +4,9 @@ import time
 
 
 def main4():
-    os.system('cls')
-    print(datetime.datetime.now().strftime('%H:%M:%S'))
-    time.sleep(1)
+    while True:
+        os.system('cls')
+        print(datetime.datetime.now().strftime('%H:%M:%S'))
+        time.sleep(1)
 
 main4()
