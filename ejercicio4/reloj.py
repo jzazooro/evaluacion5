253f69036116462a7377f83347395afff1fbbd90
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-12-20 10:25:04 +0100

    main del ejercicio4 que lo tenia ya creado de antes

--- ejercicio4/reloj.py
+++ ejercicio4/reloj.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import time
 
+
 while True:
     os.system('cls')
     print(datetime.datetime.now().strftime('%H:%M:%S'))
--- lanzador.py
+++ lanzador.py
@@ -1,4 +1,5 @@
 from ejercicio2.contador import*
+from ejercicio4.reloj import*
 
 def main2():
     contador('inc')
@@ -8,4 +9,8 @@ def main2():
     contador('')
     contador('dec')
 
-main2()
+def main4():
+    while True:
+        os.system('cls')
+        print(datetime.datetime.now().strftime('%H:%M:%S'))
+        time.sleep(1)
